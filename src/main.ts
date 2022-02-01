6d46d3602663a935734f5b65a81722d126502ec3
babenko-d-massmediagroup <mmg.babenko.d@gmail.com>
2022-02-01 10:15:25 +0200

    feature: cors added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
+  const NODE_ENV = configService.get<string>('NODE_ENV');
+  const FRONT_END_URL = configService.get<string>('FRONT_END_URL');
+
+  app.enableCors({
+    credentials: true,
+    origin: NODE_ENV === 'dev' ? '*' : FRONT_END_URL,
+  });
+
   await app.listen(PORT);
 }
 bootstrap();
